fc4d01b8a9a8a4bd0390c8efb6fe9dc206ed4b2f
haiquangtran <hai.quang.tran@gmail.com>
2015-11-21 23:02:39 +1300

    add basic folder structure Added basic folder structure. Set up files for strategy pattern.

---
+++ src/hqt/designpatterns/patterns/strategy/StrategyMain.java
@@ -0,0 +1,10 @@
+package hqt.designpatterns.patterns.strategy;
+
+public class StrategyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/hqt/designpatterns/tests/strategy.java
@@ -0,0 +1,14 @@
+package hqt.designpatterns.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class strategy {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
