dc74cb84f515678823cc98b818d633c745b68a2f
haiquangtran <hai.quang.tran@gmail.com>
2016-04-25 21:20:33 +1200

    added basic tests for command pattern

--- src/hqt/designpatterns/tests/CommandExampleTests.java
+++ src/hqt/designpatterns/tests/CommandExampleTests.java
@@ -1,27 +1,57 @@
 package hqt.designpatterns.tests;
 
 import static org.junit.Assert.*;
+
+import java.io.PrintStream;
+
 import hqt.designpatterns.patterns.command.*;
-import hqt.designpatterns.patterns.command.LightOnCommand;
-import hqt.designpatterns.patterns.command.SimpleRemoteControl;
 import hqt.designpatterns.patterns.decorator.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class CommandExampleTests {
 	private double precision = 0.0;
+	private SimpleRemoteControl remote;
+
+	@Before
+	public void setUpSimpleRemoteControl() {
+		remote = new SimpleRemoteControl();
+	}
 
 	@Test
 	public void remoteShouldTurnLightOn() {
-		SimpleRemoteControl remote = new SimpleRemoteControl();
 		Light light = new Light();
 		LightOnCommand command = new LightOnCommand(light);
 		remote.setCommand(command);
-		
+
 		assertTrue("light should be off", !light.isOn());
-		remote.buttonWasPressed();;
+		remote.buttonWasPressed();
 		assertTrue("light should now be on", light.isOn());
 	}
-	
+
+	@Test
+	public void remoteShouldOpenGarageDoor() {
+		GarageDoor garage = new GarageDoor();
+		remote.setCommand(new GarageDoorOpenCommand(garage));
+		assertTrue("light should be off", !garage.isOpen());
+		remote.buttonWasPressed();;
+		assertTrue("light should now be on", garage.isOpen());
+	}
+
+	@Test 
+	public void remoteShouldTurnOnLightsAndOpenGarageDoor() {
+		GarageDoor garage = new GarageDoor();
+		Light light = new Light();
+		
+		remote.setCommand(new LightOnCommand(light);
+		remote.buttonWasPressed();
+		assertTrue(light.isOn());
+
+		remote.setCommand(new GarageOpenCommand(garage));
+		remote.buttonWasPressed();
+		assertTrue(garage.isOpen());
+	}
+
 }
